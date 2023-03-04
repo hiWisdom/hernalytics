8fae4a7846c4e9a40507bb26b62d92534f42345b
Orodje Wisdom <69108713+hiWisdom@users.noreply.github.com>
2023-03-04 11:21:15 +0100

    Updates

---
+++ precache-manifest.5babf7c450158160033880e22221f34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd57c827b6e34405be5e",
+    "url": "/hernalytics/static/css/main.555a4430.chunk.css"
+  },
+  {
+    "revision": "dd57c827b6e34405be5e",
+    "url": "/hernalytics/static/js/main.dd57c827.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/hernalytics/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "881fb9ff2c0088fa1d26",
+    "url": "/hernalytics/static/js/2.881fb9ff.chunk.js"
+  },
+  {
+    "revision": "4c23d0851d351bcdd076",
+    "url": "/hernalytics/static/js/runtime~main.4c23d085.js"
+  },
+  {
+    "revision": "d536f86e17050ac486002ee407166e4a",
+    "url": "/hernalytics/static/media/Image10.d536f86e.png"
+  },
+  {
+    "revision": "05812cf089d3d4901996372293be0823",
+    "url": "/hernalytics/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hernalytics/precache-manifest.5babf7c450158160033880e22221f34f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hernalytics/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
