d005c13b0d45bec0f034a965d3cf55aa1317dd58
Orodje Wisdom <69108713+hiWisdom@users.noreply.github.com>
2023-03-10 08:32:25 +0100

    Updates

---
+++ precache-manifest.5dd99aeb47f8cb518069650113df536c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "35cf5c214f973cec777a",
+    "url": "/hernalytics/static/css/main.4047c1a0.chunk.css"
+  },
+  {
+    "revision": "35cf5c214f973cec777a",
+    "url": "/hernalytics/static/js/main.35cf5c21.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/hernalytics/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "3e051be04e3ecdc80553",
+    "url": "/hernalytics/static/js/2.3e051be0.chunk.js"
+  },
+  {
+    "revision": "dd47d77c932a331ee37b",
+    "url": "/hernalytics/static/js/runtime~main.dd47d77c.js"
+  },
+  {
+    "revision": "95fbc764d2f52bce77a40f5ff65f25e5",
+    "url": "/hernalytics/static/media/Countdown frame.95fbc764.png"
+  },
+  {
+    "revision": "23c3f2cd4c135907f2ba0ab103fd072e",
+    "url": "/hernalytics/static/media/shutterstock_1615848271 1.23c3f2cd.png"
+  },
+  {
+    "revision": "460d3f6555b1a8cce0a953b5745a8bf1",
+    "url": "/hernalytics/static/media/pngaaa 1.460d3f65.png"
+  },
+  {
+    "revision": "5e49e765f7a762f343266e476388057b",
+    "url": "/hernalytics/static/media/Frame1.5e49e765.png"
+  },
+  {
+    "revision": "d536f86e17050ac486002ee407166e4a",
+    "url": "/hernalytics/static/media/Image10.d536f86e.png"
+  },
+  {
+    "revision": "14a78d7fad54f2fa680fd2fd0bcdb58f",
+    "url": "/hernalytics/static/media/box2Image.14a78d7f.png"
+  },
+  {
+    "revision": "74834b36971453fd425eb679e2eec590",
+    "url": "/hernalytics/static/media/box1Image.74834b36.png"
+  },
+  {
+    "revision": "ee1ea2fc07d64b08761febce54749797",
+    "url": "/hernalytics/static/media/box3Image.ee1ea2fc.png"
+  },
+  {
+    "revision": "b8f25f2692f662ffdaba238a62811beb",
+    "url": "/hernalytics/static/media/box4Image.b8f25f26.png"
+  },
+  {
+    "revision": "a9b9e3f09dcc9f13d7e1366ddb608342",
+    "url": "/hernalytics/static/media/Get started.a9b9e3f0.png"
+  },
+  {
+    "revision": "eb9d4d2d822e52122a8bd37dc5d42f50",
+    "url": "/hernalytics/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hernalytics/precache-manifest.00dc36c3375f712592f0281b4eccd916.js"
+  "/hernalytics/precache-manifest.5dd99aeb47f8cb518069650113df536c.js"
 );
 
 workbox.clientsClaim();
