2e22241cd4aca345034a63760cfc1879f6a704e5
Orodje Wisdom <69108713+hiWisdom@users.noreply.github.com>
2023-03-10 23:34:23 +0100

    update on landing page slide show

--- src/components/BasicSlider.js
+++ src/components/BasicSlider.js
@@ -1,13 +1,14 @@
 
 //import react
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 //import stylesheet
 import './../App.css';
 import './main.css';
 
 //import slide-show component and function
-import HeroSlider, {Slider} from 'hero-slider';
+// import HeroSlider, {Slider} from 'hero-slider';
+import imageSlide from './imgdata'
 
 
 //images
@@ -21,14 +22,47 @@ import{ FaChevronCircleDown } from 'react-icons/fa';
 
 
 const App =  () => {
+
+    const[currentState, setCurrentState] = useState(0)
+    useEffect(()=>{
+        const timer = setTimeout(()=> {
+            if(currentState===2) {
+                setCurrentState(0)
+            }
+            else{
+                setCurrentState(currentState + 1)
+            }
+
+        }, 1200)
+        return () => clearTimeout(timer);
+    }, [currentState])
+
+    const bgImagesStyle =  {
+        backgroundImage: `url(${imageSlide[currentState].url})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100%',
+        width: '700px',
+        marginTop: '2.5%'
+    }
+
     return (
         <div className="main_block_content main_block_content2">
+
+
+
         <div className="mapDropDown">
           <select>
             <option>Nigeria <FaChevronCircleDown /></option>
           </select>
         </div>
-        <img src={Map1} className="MapImages"/>
+        {/* <img src={Map1} className="MapImages"/> */}
+        <div className="MapImages" style={bgImagesStyle} ></div>
+
+
+
+
+
       </div>
     )
 }
---
+++ src/components/imgdata.js
@@ -0,0 +1,18 @@
+
+
+const imageSlide = [
+
+    {
+        url: 'Frame1.png'
+    },
+
+    {
+        url: 'Frame2.png'
+    },
+
+    {
+        url: 'Frame3.png'
+    }
+]
+
+export default imageSlide;
